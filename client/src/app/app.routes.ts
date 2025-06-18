ad3c5b2a9d3ddfbfcf4abf35bd17fe02477d49f9
GovindaKunhale <gsk26042000@gmail.com>
2025-06-19 00:34:02 +0530

    authGuard, nav comp css[bootswatch], added 3 comp

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home.component/home.component';
+import { MemberDetailComponent } from './members/member-detail.component/member-detail.component';
+import { MessagesComponent } from './components/messages.component/messages.component';
+import { ListsComponent } from './components/lists.component/lists.component';
+import { MemberListComponent } from './members/member-list.component/member-list.component';
+import { authGuard } from './_guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'members/:id', component: MemberDetailComponent },
+            { path: 'lists', component: ListsComponent },
+            { path: 'messages', component: MessagesComponent }
+        ]
+    },
+
+    { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
---
+++ client/src/app/members/member-list.component/member-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list.component',
+  imports: [],
+  templateUrl: './member-list.component.html',
+  styleUrl: './member-list.component.css'
+})
+export class MemberListComponent {
+
+}
