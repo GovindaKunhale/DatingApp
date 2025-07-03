5b2ec372edc7d06c4d4018042a7530f8f10f770f
GovindaKunhale <gsk26042000@gmail.com>
2025-07-03 22:53:59 +0530

    error handling, interceptor

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MessagesComponent } from './components/messages.component/messages.comp
 import { ListsComponent } from './components/lists.component/lists.component';
 import { MemberListComponent } from './members/member-list.component/member-list.component';
 import { authGuard } from './_guards/auth-guard';
+import { TestErrorsComponent } from './errors/test-errors.component/test-errors.component';
+import { NotFoundComponent } from './errors/not-found.component/not-found.component';
+import { ServerErrorComponent } from './errors/server-error.component/server-error.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -21,5 +24,8 @@ export const routes: Routes = [
         ]
     },
 
-    { path: '**', component: HomeComponent, pathMatch: 'full' },
+    {path: 'errors', component: TestErrorsComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: 'server-error', component: ServerErrorComponent},
+    { path: '**', component: HomeComponent, pathMatch: 'full' }
 ];
